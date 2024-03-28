ddf805136ce47e87afcbc564f9db57fdae78bfa2
Johny Messias <jmessias@everis.nttdata.com>
2024-03-28 04:38:00 -0300

    🚧 - Corrigindo erro

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -14,47 +14,49 @@ class LoginView extends StatelessWidget {
     return Form(
       key: _formKey,
       child: Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(left: 6, top: 83),
-              child: Text(
-                titlePage,
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(left: 6, top: 83),
+                child: Text(
+                  titlePage,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 30),
-            CustomTextFormFild(
-              hintText: 'Enderço de email para logar',
-              keyboardType: TextInputType.emailAddress,
-              validator: (value) => (value != null && value.isEmpty) ? 'Digite um email válido!' : null,
-            ),
-            const SizedBox(height: 20),
-            CustomTextFormFild(
-              hintText: 'Digite sua senha',
-              obscureText: true,
-              validator: (value) => (value != null && value.isEmpty) ? 'Digite uma senha!' : null,
-            ),
-            const SizedBox(height: 40),
-            CustomButton(text: 'Entrar', onPressed: (){
-              if(_formKey.currentState!.validate()) {
-                print('Campos validos');
-              }
-            }),
-            const SizedBox(height: 20),
-            CustomButton(text: 'Registrar', onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (newContext) => const RegisterView(),
-                ),
-              );
-            })
-          ],
+              const SizedBox(height: 30),
+              CustomTextFormFild(
+                hintText: 'Enderço de email para logar',
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) => (value != null && value.isEmpty) ? 'Digite um email válido!' : null,
+              ),
+              const SizedBox(height: 20),
+              CustomTextFormFild(
+                hintText: 'Digite sua senha',
+                obscureText: true,
+                validator: (value) => (value != null && value.isEmpty) ? 'Digite uma senha!' : null,
+              ),
+              const SizedBox(height: 40),
+              CustomButton(text: 'Entrar', onPressed: (){
+                if(_formKey.currentState!.validate()) {
+                  print('Campos validos');
+                }
+              }),
+              const SizedBox(height: 20),
+              CustomButton(text: 'Registrar', onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (newContext) => const RegisterView(),
+                  ),
+                );
+              })
+            ],
+          ),
         ),
       ),
     );
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -13,47 +13,49 @@ class RegisterView extends StatelessWidget {
     return Form(
       key: _formKey,
       child: Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(left: 6, top: 83),
-              child: Text(
-                titlePage,
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(left: 6, top: 83),
+                child: Text(
+                  titlePage,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 30),
-            CustomTextFormFild(
-              hintText: 'Enderço de email para logar',
-              keyboardType: TextInputType.emailAddress,
-              validator: (value) => (value != null && value.isEmpty)
-                  ? 'Digite um email válido!'
-                  : null,
-            ),
-            const SizedBox(height: 20),
-            CustomTextFormFild(
-              hintText: 'Digite sua senha',
-              obscureText: true,
-              validator: (value) =>
-                  (value != null && value.isEmpty) ? 'Digite uma senha!' : null,
-            ),
-            const SizedBox(height: 40),
-           CustomButton(text: 'Registrar', onPressed: (){
-               if (_formKey.currentState!.validate()) {
-                 print('Campos válidos');
-               }
-             },
-           ),
-            const SizedBox(height: 20),
-            CustomButton(text: 'Logar', onPressed: () {
-              Navigator.pop(context);
-            },
-            )
-          ],
+              const SizedBox(height: 30),
+              CustomTextFormFild(
+                hintText: 'Enderço de email para logar',
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) => (value != null && value.isEmpty)
+                    ? 'Digite um email válido!'
+                    : null,
+              ),
+              const SizedBox(height: 20),
+              CustomTextFormFild(
+                hintText: 'Digite sua senha',
+                obscureText: true,
+                validator: (value) =>
+                    (value != null && value.isEmpty) ? 'Digite uma senha!' : null,
+              ),
+              const SizedBox(height: 40),
+             CustomButton(text: 'Registrar', onPressed: (){
+                 if (_formKey.currentState!.validate()) {
+                   print('Campos válidos');
+                 }
+               },
+             ),
+              const SizedBox(height: 20),
+              CustomButton(text: 'Logar', onPressed: () {
+                Navigator.pop(context);
+              },
+              )
+            ],
+          ),
         ),
       ),
     );
